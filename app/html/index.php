5ea7b44e956b5321f48039f1f28ad3148543528a
Felix <felix@lixdev.de>
2020-01-18 15:16:34 +0100

    Update index.php

--- app/html/index.php
+++ app/html/index.php
@@ -21,15 +21,24 @@
 	</header>
 	<main>
 		<?php
+		ini_set('display_errors', 'Off');
 		$conn = new mysqli("db", "dev-user", "dev-password");
 		$db = "true";
 		if ($conn->connect_error) $db = "false";
+		ini_set('display_errors', 'On');
 		?>
 		<div class="f">
 			<div class="m">MariaDB: <span class="s <?= $db ?>"></span></div>
 			<div class="p">PHP: <span><?= phpversion() ?></span></div>
 			<div class="p"><a href="http://localhost:8080" target="blank">phpMyAdmin</a></div>
 		</div>
+		<?php
+		if ($conn->connect_error) {
+			echo "<h3>MariaDB connection failed:</h3> ";
+			echo "<p>" . $conn->connect_error . "</p>";
+			die();
+		}
+		?>
 		<h3>Your Webserver is ready!</h3>
 		<p>Now you can start to code.</p>
 	</main>
