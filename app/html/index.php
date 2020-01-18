e5219a1124700bc5e51ccc2b1f8e8e7aa35a71ad
Felix <felix@lixdev.de>
2020-01-18 13:58:06 +0100

    First commit

---
+++ app/html/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Docker</title>
+</head>
+
+<body>
+	<h1>Webserver ist running! You can run your Application.</h1>
+</body>
+
+</html>
---
+++ readme.md
@@ -0,0 +1,16 @@
+# Docker development compose
+
+##  Container
+
+> **Apache2**
+> **PHP 7.3**
+> **MariaDB**
+> **phpmyadmin**
+
+### Included in Apache container
+> Composer
+> Nano
+> NodeJS
+> Git
+> Zip / Unzip
+> Screen
